c8393d91e5499d27ba69bd90f030a4766e975008
Nikolay Voynov <nvoynov@gmail.com>
2021-12-18 13:32:29 +0200

    github workflow

--- test/services/select_users_spec.rb
+++ test/services/select_users_spec.rb
@@ -7,7 +7,7 @@ When I rewrote services tests using mocks and stubs and then threw away MemoryGa
 
 The RegisterUser, AuthenticateUser, and ChangeUserPassword services are "Commands", that really have some business logic. At the same time,  SelectUsers is just a "Query" that has no logic and just plays as a bridge between the service's consumer and the data gateway; it just passes params to the gateway and returns the gateway response to the consumer.
 
-So, in this case, for this particular "query" service, only argument checing policies should be tested, and maybe the fact that service just return the gateway answer. And creating a gateway for test purposes at the domain business logic level might be just waste of time, but let's look into it again at the interface layer of the domain service.
+So, in this case, for this particular "query" service, only argument checking policies should be tested, and maybe the fact that service just return the gateway answer. And creating a gateway for test purposes at the domain business logic level might be just waste of time, but let's look into it again at the interface layer of the domain service.
 =end
 
 describe SelectUsers do
